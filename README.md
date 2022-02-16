ab093490ce04fbff6308ece832c67ced852d47c6
Amy Churchwell <achurchwell@mozilla.com>
2022-02-15 17:06:21 -0800

    debug mismatch of sick hours

---
+++ README.md
@@ -0,0 +1,6 @@
+# Check sick time accrual
+
+> One hour of PSL for every 30 hours worked, and will accrue up to a maximum of 72 hours.
+
+1. Go to ADP > My Pay
+2. Select all pay statements to download. **note:** ADP seems to only allow 10 at a time. This script will assume all PDFs are in the same `pay_statements` folder inside this directory.
--- checker.py
+++ checker.py
@@ -1,12 +1,36 @@
-import PyPDF2, os
+import PyPDF2, os, re, datetime, operator
 
-pdfFiles = []
+payStatements = []
 
 for filename in os.listdir('./pay_statements/'):
     if filename.endswith('.pdf'):
+        # collect all pay statements and extract text
         pdfFileObj = open('./pay_statements/' + filename, 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
         pageObj = pdfReader.getPage(0)
         text = pageObj.extractText()
-        pdfFiles.append(text)
-        print(pdfFiles)
+
+        # collect pay dates
+        dateRegex = re.compile(r'\d{4}-\d{2}-\d{2}')
+        dateString = dateRegex.search(filename).group(0)
+
+        # search for available sick time
+        cleanCopy = re.sub(r'\n', ' ', text)
+        sickTimeRegex = re.compile(r'(?<=Sick Available )\d{2} \d{2}')
+        isSickTime = sickTimeRegex.search(cleanCopy)
+
+        # sick time cannot be higher than 72, if it is the text extraction errored.
+        # the error pattern seems to be that sick hours are trailing "Other Benefits and "
+        if isSickTime.group(0) > '72 00':
+            errorRegex = re.compile(r'(?<=Other Benefits and )\d{2} \d{2}')
+            isSickTime = errorRegex.search(cleanCopy)
+
+        payStatements.append({'date': dateString, 'sick hours': isSickTime.group(0)})
+
+# sort by date
+payStatements.sort(key = operator.itemgetter('date'))
+
+# known bug: when sick hours are maxed, check that following pay period hours are not
+# suspended below 72 consecutively.
+
+print(payStatements)
